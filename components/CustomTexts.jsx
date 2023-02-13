2cf79d1e06379cc244dc2440337afa7b772679d4
Wilson Pinto <wilsonpinto88@gmail.com>
2023-02-13 15:31:45 +0000

    added Explore section

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,28 +1,36 @@
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-template-curly-in-string */
 /* eslint-disable react/jsx-curly-brace-presence */
 // eslint-disable-next-line lines-around-directive
-'use client';
+"use client";
 
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
-import { textContainer, textVariant2 } from '../utils/motion';
+import { textContainer, textVariant2 } from "../utils/motion";
 
 export const TypingText = ({ title, textStyles }) => (
   <motion.p
     variants={textContainer}
-    className={'font-normal text-[14px] text-secondary-white ${textStyles}'}
+    className={"font-normal text-[14px] text-secondary-white ${textStyles}"}
   >
     {Array.from(title).map((letter, index) => (
-      <motion.span
-        variants={textVariant2}
-        key={index}
-      >
-        {letter === ' ' ? '\u00A0' : letter}
+      <motion.span variants={textVariant2} key={index}>
+        {letter === " " ? "\u00A0" : letter}
       </motion.span>
     ))}
   </motion.p>
 );
 
 export const TitleText = ({ title, textStyles }) => (
-  <h2>Title Text</h2>
+  <motion.h2
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={
+      "mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}"
+    }
+  >
+    {title}
+  </motion.h2>
 );
