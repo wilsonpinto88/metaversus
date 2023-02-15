360f039cd9e637b1af18ed9e0c9f681763dc667b
Wilson Pinto <wilsonpinto88@gmail.com>
2023-02-15 16:45:49 +0000

    added the Insight section

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,8 +1,36 @@
-'use client';
+/* eslint-disable lines-around-directive */
+/* eslint-disable quotes */
+"use client";
+
+import { motion } from "framer-motion";
+
+// eslint-disable-next-line no-unused-vars
+import styles from "../styles";
+import { staggerContainer } from "../utils/motion";
+import { InsightCard, TitleText, TypingText } from "../components";
+import { insights } from "../constants";
 
 const Insights = () => (
-  <section>
-    Insights section
+  <section className={`${styles.paddings}  relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: "false", amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| Insight" textStyles="text-center" />
+      <TitleText title="Insight about metaverse" textStyles="text-center" />
+      <div className="mt-[15px] flex flex-col gap-[30px]">
+        {insights.map((insight, index) => (
+          <InsightCard
+            key={`insight-${index}`}
+            {...insight}
+            index={index + 1}
+          />
+        ))}
+      </div>
+    </motion.div>
   </section>
 );
 
